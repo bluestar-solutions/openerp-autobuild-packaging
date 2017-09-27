9a58086ef9e1b3f5108db3b4bf45479d95b54739
Hervé Martinet <herve.martinet@bluestar.solutions>
2017-09-27 12:05:36 +0200

    Fix dependencies

---
+++ jessie/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Clear out the /build and /release directory
+rm -rf /build/*
+rm -rf /release/*
+
+# Re-pull the repository
+git clone https://github.com/bluestar-solutions/openerp-autobuild.git src
+cd src
+git checkout develop
+cp -R /root/debian .
+
+dch -M -l "~b`date +%Y%m%d%H%M`~jessie" "Build `date +%Y%m%d%H%M`"
+dch -M -r --distribution jessie-backports ""
+cp debian/changelog /release/
+debuild -i -us -uc -b
+cd ..
+cp *.deb /release/
